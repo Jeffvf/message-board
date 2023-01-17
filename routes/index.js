f1be3f9ef95a8cdd36ffadda55de713fec101970
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-17 13:42:45 -0300

    feat: mostra mensagens de usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "hi",
+    user: "test-user",
+    added: new Date()
+  }
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
 
 module.exports = router;
