37862c9a3291ea7a1eb812e623df63d83ca18162
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-17 14:07:03 -0300

    feat: forms para adicionar novas mensagens

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message Board', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'Form'});
+});
+
+router.post('/new', function(req, res, next) {
+  const message = req.body.message;
+  const username = req.body.username;
+
+  messages.push({text: message, user: username, added: new Date()});
+
+  res.redirect('/');
+})
+
 module.exports = router;
